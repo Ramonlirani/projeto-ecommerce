599f47ae5bea40f74bd258a1fcff0068cebf0509
ramonlirani <ramonlirani@hotmail.com>
2024-01-02 15:34:53 -0300

    [FRONTEND] fix: changed name data-tooltip-id

--- frontend/src/components/shared/table/actions/edit-button.tsx
+++ frontend/src/components/shared/table/actions/edit-button.tsx
@@ -19,14 +19,14 @@ export function EditButton({ url, id, modelName }: EditButtonProps) {
       <Link
         href={`/system/${url}/form/${id}`}
         className="mr-2"
-        data-tooltip-id={`edit-album-${id}`}
+        data-tooltip-id={`edit-${id}`}
         data-tooltip-content="Editar"
       >
         <span className="inline-flex items-center rounded-md bg-indigo-100 px-2 py-1 text-indigo-700">
           <PencilIcon className="h-5 w-5" aria-hidden="true" />
         </span>
       </Link>
-      <Tooltip id={`edit-album-${id}`} />
+      <Tooltip id={`edit-${id}`} />
     </>
   ) : null;
 }
