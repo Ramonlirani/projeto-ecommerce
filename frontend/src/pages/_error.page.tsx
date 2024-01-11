a782899c065c8b3bacad07c2397054cf894e98ff
ramonlirani <ramonlirani@hotmail.com>
2024-01-10 22:33:46 -0300

    [FRONTEND] style: changed colors error page

--- frontend/src/pages/_error.page.tsx
+++ frontend/src/pages/_error.page.tsx
@@ -30,7 +30,7 @@ function Error({ statusCode }: ErrorProps) {
       )}
 
       <Link href="/" className="mt-4">
-        <div className="flex gap-3 items-center text-tomilho">
+        <div className="flex gap-3 items-center text-off-red">
           <ArrowLeftIcon width={20} /> voltar
         </div>
       </Link>
@@ -40,7 +40,7 @@ function Error({ statusCode }: ErrorProps) {
         aria-hidden="true"
       >
         <div
-          className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-tomilho to-tomilho opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-off-red to-off-red opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
           style={{
             clipPath:
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
